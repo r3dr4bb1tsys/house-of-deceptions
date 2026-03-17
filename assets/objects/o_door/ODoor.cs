9200dea928574d40c28c6d1f76a4c0decf1c9405
r3dr4bb1tsys <lavalleleonardo23@gmail.com>
2026-03-17 23:18:50 +0100

    Fixed and completed the door system and did a clean up of the code.

--- assets/objects/o_door/ODoor.cs
+++ assets/objects/o_door/ODoor.cs
@@ -1,11 +1,8 @@
 using Godot;
-using System;
-
 public partial class ODoor : Node2D
 {
 	// # Private Variables # //
 	private Area2D _doorInteractionArea;
-	private bool _readyToTeleport;
 	private ODoor _linkedDoor;
 	// # ----------------- # //
 
@@ -16,7 +13,7 @@ public partial class ODoor : Node2D
 	// # ---------------- # // 
 
 	// # Export Varianles # //
-	[Export] ODoor LinkedDoor
+	[Export] public ODoor LinkedDoor
 	{
 		get => _linkedDoor;
 		set
@@ -29,20 +26,19 @@ public partial class ODoor : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// check for the door's interaction area existence, if none is found return an error message, otherwise, connect the body_entered and body_exited signals of the area.
+		
+		// check for the door's interaction area existence, if none is found return an error message.
 		_doorInteractionArea = FindChild("doorInteractionArea") as Area2D;
 		if (_doorInteractionArea == null)
 		{
-			GD.PrintErr($"[Ready] - Failed to find the interaction area for the door: [{this.Name}]. Make sure to add one. ");
+			GD.PrintErr($"[ODoor.cs/Ready] - Failed to find the interaction area for the door: [{this.Name}]. Make sure to add one. ");
 			return;
 		}
-		_doorInteractionArea.Connect("area_entered", new Callable(this, nameof(InteractionAreaEntered)));
-		_doorInteractionArea.Connect("area_exited", new Callable(this, nameof(InteractionAreaExited)));
-
-		// check for the existence of a linked door, if none is found return an error message, otherwise, use the linked door's marker position as the exit position for the player teleportation.
+		
+		// check for the existence of a linked door, if none is found return an error message.
 		if (_linkedDoor == null)
 		{
-			GD.PrintErr($"[Ready] - Failed to find a linked door for [{this.Name}]. Make sure to assign one through the inspector.");
+			GD.PrintErr($"[ODoor.cs/Ready] - Failed to find a linked door for [{this.Name}]. Make sure to assign one through the inspector.");
 			return;
 		}
 
@@ -50,59 +46,8 @@ public partial class ODoor : Node2D
 		_localExitMarker = FindChild("localExitPositionMarker", true) as Marker2D;
 		if (_localExitMarker == null)
 		{
-			GD.PrintErr($"[Ready] - Failed to find a local exit marker for door: [{this.Name}]. Make sure to add one as a child of the door.");
+			GD.PrintErr($"[ODoor.cs/Ready] - Failed to find a local exit marker for door: [{this.Name}]. Make sure to add one as a child of the door.");
 			return;
 		}
 	}
-
-    // # Input # //
-    public override void _Input(InputEvent @event)
-    {
-		// Handle the door's interaction input. 
-		if (@event.IsActionPressed(OPlayer.PlayerActions.Interact))
-		{
-			// check the door is reade to teleport the player, if not, return an error message. This is crucial as we dont want the player to be teleported even if they're not colliding with any door.
-			if (_readyToTeleport == false) //! // FIXME: This check returns false even if the player is colliding with the door and the _readyToTeleport variable is set to true, i think this is because it checks for all the door's _readyToTeleport variables and not just the one that the player is colliding with, need to find a way to check for the specific door that the player is colliding with instead of all the doors ins the scene. 
-			{
-				GD.PrintErr($"[Interaction Input] - The door is not ready to teleport the player as it's not colliding with it. returning...");
-				return;
-			}
-
-			// Search for a player inside the current scene root. This is crucial for the door teleportation to work, as we need to access the player's teleportation function and position. If no player is found, return an error message.
-			OPlayer player = GetTree().Root.FindChild("oPlayer", true) as OPlayer;
-			if (player == null)
-			{
-				GD.PrintErr($"[Interaction Input] - Failed to find the player node inside the scene: [{GetTree().CurrentScene.Name}]. Make sure the player is instanced in the current scene.");
-				return;
-			}
-
-			// teleport the player to the new position.
-			Vector2 newPosition = _linkedDoor._localExitMarker.Position;
-			GD.Print($"[Interaction Input] - Teleporting player to the new door's position: [{newPosition}]");
-			player.TeleportTo(newPosition);
-		}
-    }
-
-
-	// # Signals # //
-	private void InteractionAreaEntered(Area2D area)
-	{
-		GD.Print(new string('-', 100));
-		GD.Print($"[Area entered signal] - The area [{area.Name}] has entered [{this.Name}]'s interaction area.");
-		GD.Print($"[{area.Name}]'s groups: {string.Join(",", area.GetGroups())}");
-		if (area.IsInGroup("PlayerInteactionArea"))
-		{
-			_readyToTeleport = true;
-			GD.Print($"[Area entered signal] - The player is colliding with door: [{this.Name}].");
-		}
-		GD.Print($"[Area entered signal] -  _readyToTeleport has been set to: {_readyToTeleport}\n" + new string('-', 100) + "\n");
-	}
-	private void InteractionAreaExited(Area2D area)
-	{
-		_readyToTeleport = false;
-		GD.Print(new string('-', 100));
-		GD.Print($"[Area exited signal] - The player is no longer colliding with door: [{this.Name}].");
-		GD.Print($"[Area exited signal] - _readyToTeleport has been set to: {_readyToTeleport}\n" + new string('-', 100) + "\n");	
-	}
-	// # -------- # //
 }
--- assets/objects/o_player/OPlayer.cs
+++ assets/objects/o_player/OPlayer.cs
@@ -1,24 +1,52 @@
-using Godot;
-using System;
+// TODO: This is a generic todo, so it applied to all the .cs files in the project, please refactor the code so it's more clean and readable, add more comments where needed and remove any redundant code. 
 
+using Godot;
 public partial class OPlayer : CharacterBody2D
 {
 
-	// # --------public variables-----------#
-	// # ------------------------------------#
+	/// <summary>
+	/// Standard player actions for movement and interaction. These should be set up in the project input map for the player to work properly.
+	/// </summary>
+	public struct PlayerActions
+	{
+		public const string MoveRight = "playerMoveRight";
+		public const string MoveLeft = "playerMoveLeft";
+		public const string MoveUp = "playerMoveUp";
+		public const string MoveDown = "playerMoveDown";
+		public const string Interact = "playerInteract";
+	}
 
-// # -----------Exports------------------#
+	// # Private Variables # //
+	private ODoor _enteredDoor;
+	// # ----------------- # //
+
+	// # public variables # // 
+	// this variable is used to check wether the player is ready for teleportation or not, this is used in door transportation for example. 
+	public bool _readyToTeleport = false;
+	// # ---------------- # //
+
+	// # Exports # //
 	// Speed Variable
 	[Export] public int Speed = 50;
-    // # --------------------------------#
+	// # ------- # //
 
     public override void _Ready()
     {
-		// Check if the playerInteractionArea is inside the player, if not return an error message.
+		// check if the playerInteactionArea is inside the player, if not return an error message.
+		Area2D playerInteactionArea = FindChild("playerInteactionArea", true) as Area2D;
+		if (playerInteactionArea == null)
+		{
+			GD.PrintErr($"[OPlayer.cs/_Ready] - Failed to find the interaction area for the player [{this.Name}]. Make sure to add one as a child of the player.");
+			return;
+		}
+		playerInteactionArea.Connect("area_entered", new Callable(this, nameof(InteractionAreaEntered)));
+		playerInteactionArea.Connect("area_exited", new Callable(this, nameof(InteractionAreaExited)));
+
+		// Check if the playerInteractionShape is inside the player, if not return an error message.
 		CollisionShape2D playerInteractionShape = FindChild("playerInteractionShape", true) as CollisionShape2D;
 		if (playerInteractionShape == null)
 		{
-			GD.PrintErr($"Failed to find the player interaction shape for the player [{this.Name}]. Make sure to add one as a child of the player.");
+			GD.PrintErr($"Failed to find the interaction shape for the player [{this.Name}]. Make sure to add one as a child of the player.");
 			return;
 		}
     }
@@ -35,6 +63,51 @@ public partial class OPlayer : CharacterBody2D
 		MoveAndSlide();
 	}
 
+	public override void _Input(InputEvent @event)
+    {
+		// Handle the door's interaction input. 
+		if (@event.IsActionPressed(PlayerActions.Interact))
+		{
+			// check the door is reade to teleport the player, if not, return an error message. This is crucial as we dont want the player to be teleported even if they're not colliding with any door.
+			if (this._readyToTeleport == false)
+			{
+				GD.PrintErr($"[OPlayer.cs/Interaction Input] - The player is not ready for teleportation as it's not colliding with a valid door. this._readyToTeleport: {this._readyToTeleport}\t returning...");
+				return;
+			}
+
+			// teleport the player to the new position.
+			Vector2 newPosition = _enteredDoor.LinkedDoor._localExitMarker.GlobalPosition;
+			GD.Print($"[OPlayer.cs/_Input] - Teleporting player to the new door's position: [{newPosition}]");
+			this.TeleportTo(newPosition);
+		}
+    }
+	
+
+	// # Signals # //
+	private void InteractionAreaEntered(Area2D area)
+	{
+		// get the door from wich is parent of the area that entered the player interaction area.
+		_enteredDoor = area.GetParent() as ODoor;
+
+		GD.Print(new string('-', 100));
+		GD.Print($"[OPlayer.cs/Area entered signal] - The area [{area.Name}] has entered [{this.Name}]'s interaction area.");
+		GD.Print($"[OPlayer.cs/Area entered signal] - [{_enteredDoor.Name}]'s groups: {string.Join(",", _enteredDoor.GetGroups())}");
+		if (_enteredDoor.IsInGroup("Door"))
+		{
+			this._readyToTeleport = true;
+			GD.Print($"[OPlayer.cs/Area entered signal] - The player is colliding with door: [{_enteredDoor.Name}].");
+		}
+		GD.Print($"[OPlayer.cs/Area entered signal] - this._readyToTeleport has been set to: {this._readyToTeleport}\n" + new string('-', 100) + "\n");
+	}
+	private void InteractionAreaExited(Area2D area)
+	{
+		this._readyToTeleport = false;
+		GD.Print(new string('-', 100));
+		GD.Print($"[OPlayer.cs/Area exited signal] - The player is no longer colliding with a door.");
+		GD.Print($"[OPlayer.cs/Area exited signal] - this._readyToTeleport has been set to: {this._readyToTeleport}\n" + new string('-', 100) + "\n");	
+	}
+	// # -------- # //
+
 	/// <summary>
 	/// Standard fucntion for player teleportation. 
 	/// </summary>
@@ -43,17 +116,4 @@ public partial class OPlayer : CharacterBody2D
 	{
 		Position = newPosition;
 	}
-
-	/// <summary>
-	/// Standard player actions for movement and interaction. These should be set up in the project input map for the player to work properly.
-	/// </summary>
-	public struct PlayerActions
-	{
-		public const string MoveRight = "playerMoveRight";
-		public const string MoveLeft = "playerMoveLeft";
-		public const string MoveUp = "playerMoveUp";
-		public const string MoveDown = "playerMoveDown";
-		public const string Interact = "playerInteract";
-	}
-
 }
